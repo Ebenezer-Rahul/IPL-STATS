52069da78e39f90bc7a9ebb890cd277893ed4ef2
ebenezer rahul <ebenezerrahul@gmail.com>
2022-11-11 21:55:17 +0530

    a good start

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 #from flask_mysqldb import MySQL
 import mariadb
-
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -13,12 +13,51 @@ connection = mariadb.connect(
          database='sys')
 cur = connection.cursor()
 
-cur.execute('INSERT INTO Student VALUES (28, "Eren yeager");')
-connection.commit()
+
 
 @app.route('/')
 def index():
-    return '<h1> Rahul the server is working for real<h1>'
+
+    cur.execute('SELECT * FROM Team ORDER BY pos');
+    print(cur)
+    teams = cur.fetchall()
+    connection.commit()
+    for team in teams:
+        print(team)
+    return render_template("home.html", teams=teams, i = 0);
+
+
+@app.route('/matches')
+def matches():
+    cur.execute('SELECT * FROM Match_ ORDER BY time DESC')
+    matches = cur.fetchall()
+    connection.commit()
+    print(matches[0])
+    for match in matches:
+        s = match[1].strftime("%A %d %B %Y")
+        #match[1] = s
+        print(match[1])
+    return render_template("matches.html", matches=matches);
+
+@app.route('/match')
+def match():
+
+    return render_template("home.html");
+
+@app.route('/players')
+def players():
+    return render_template("home.html");
+
+@app.route('/player/pid=<int:pid>')
+def player(pid):
+    print(pid)
+    return render_template("home.html");
+
+
+@app.route('/deliveries')
+def deliveries():
+    return render_template("home.html");
+
 
 
 if __name__ == ' __main__':
